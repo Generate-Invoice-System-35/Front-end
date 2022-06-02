71c5c74623c2d82f8cd1c1e2a695de63f94ad8f0
Kristina Silitonga <kristinasilitonga825@gmail.com>
2022-06-02 18:04:08 +0700

    feat: adding navbar & Hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hero">
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h3>
+                <strong>Find the convenience of <br/> online invoicing here</strong>
+                <br />
+              </h3>
+              <p>
+                Online invoicing generator that helps you to create <br /> professional
+                invoices and send payment reminders.
+              </p>
+              <router-link class="btn btn-lg btn-success" to="/products">
+                Start Now. it's Free
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/images/logo.png" width="30%" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroVue",
+};
+</script>
+
+<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light">
+      <b-navbar-brand href="#"><strong>Fatture</strong></b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/keranjang">
+            Home
+          </router-link>
+        </li> 
+        <li class="nav-item">
+          <router-link class="btn btn-lg btn-success mr-2" to="/keranjang">
+            Sign In
+          </router-link>
+        </li> <br/>
+        <li class="nav-item">
+          <router-link class="btn btn-lg btn-success" to="/keranjang">
+            Sign Up
+          </router-link>
+        </li>
+      </ul>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style></style>
