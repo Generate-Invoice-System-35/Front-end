54289df5fbf02a6fbd012f9d871e4013ba9c659c
Kristina Silitonga <kristinasilitonga825@gmail.com>
2022-06-02 21:19:00 +0700

    feat:editing hero

---
+++ src/components/CardVue.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card" style="width: 13rem;">
+  <div class="card-body">
+    <h5 class="card-title">01</h5>
+    <h6 class="card-subtitle mb-2 text-muted"><strong>Create professional invoices</strong></h6> <hr/>
+  </div>
+
+
+  
+</div>
+</template>
+
+<script>
+export default {
+name: 'CardVue'
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="home">
     <Navbar />
+    <div class="container">
       <Hero />
+    </div>
+
+    <div class="row mb-3">
+      <div class="col-md-3 mt-4">
+        <CardVue />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
+import CardVue from "@/components/CardVue.vue";
 
 export default {
   name: "HomeView",
   components: {
     Navbar,
     Hero,
+    CardVue,
   },
 };
 </script>
