9b266722e9103c2f7a77753d9bf301039cac5f49
Kristina Silitonga <kristinasilitonga825@gmail.com>
2022-06-22 21:27:34 +0700

    adding navbar

---
+++ src/views/CreateInvoice.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light">
+      <b-navbar-brand href="#">
+        <img src="../assets/images/logo_fatture.png" width="150px" />
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <ul class="navbar-nav ml-auto mr-3">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/"> Home </router-link>
+        </li>
+
+        <b-dropdown
+          id="dropdown-right"
+          right
+          text="Username"
+          variant="outline-dark"
+          class="m-2"
+          size="sm"
+        >
+          <b-dropdown-item href="/">Logout</b-dropdown-item>
+        </b-dropdown>
+      </ul>
+    </b-navbar>
+    <br />
+
+    <hr />
+    <div class="container">
+      <router-link
+        class="btn-lg btn-warning"
+        style="background-color: #ff9f71; color: white;;"
+        to="/"
+      >
+        Confirm
+      </router-link>
+    </div>
+    <hr/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateInvoice",
+};
+</script>
+
+<style></style>
