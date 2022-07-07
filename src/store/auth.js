be6f4f65d3a9ac00f74ec795b85dad2d644296c1
GungdeAngga <agungangga2001@gmail.com>
2022-07-07 14:12:24 +0800

    auth udh selesai tapi masih error

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,1 +1,73 @@
-import axios from "axios";
+import axios from 'axios';
+import { LayoutPlugin } from 'bootstrap-vue';
+const apiHost = "http://api.calorilin.me";
+
+const state = () => ({
+    token: [],
+    info: '',
+  });
+
+const mutations = {
+  setToken(state, param) {
+    state.token = param;
+  },
+  setInfo(state, param) {
+    state.info = param;
+  },
+};
+
+const actions = {
+    register(state, cerdentials) {
+        cons result = axios
+        .post(
+            `$(apiHost)/register`, {
+                name: credencials.name,
+                username: cerdentials.username,
+                password: cerdentials.password,
+                email: cerdentials.email,
+                number_phone: cerdentials.number_phone,
+                address: cerdentials.address,
+            }
+        )
+        .then((response) => {
+            console.log("respon: ", response);
+            if (response.status === 201) {
+                returnt true;
+            } else {
+                store.commit("SetInfo", response.message);
+            }
+        })
+        .catch((error) => {
+            console.log("error nya adalah", error);
+            store.commit("setInfo", error);
+        });
+
+        return result;
+
+        login(store, credencials) {
+            return axios
+            .post(`${apiHost}/login`, {
+                username: cerdentials.username,
+                password: credencials.password,
+            })
+            .then((response) => {
+                if (response.status === 208) {
+                    store.commit("setToken", response.data.data);
+                    console.log(response.data);
+                    return response;
+                } else {
+                    store.commit("setInfo", response.data.message);
+                }
+            });
+        },
+        logout(state){
+            store.commit("setToken", "");
+            return true;
+        },
+    };
+    export default {
+        state,
+        mutations,
+        actions,
+    }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import auth from "./auth";
 import createPersistedState from "vue-persistedstate";
 
 Vue.use(Vuex);
 const persistedDataState = createPersistedState({
     paths: ["auth.token"],
+});
+export default new Vue.store({
+    plugins: [persistedDataState],
+    module: {
+        auth: {
+            namespaced: true,
+            ...auth,
+        },
+    },
 });
