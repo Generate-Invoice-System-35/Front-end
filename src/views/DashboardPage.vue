91a3b7e8745e5e0df9242b301696c17b4ef73583
GungdeAngga <agungangga2001@gmail.com>
2022-07-08 13:00:57 +0800

    benerin navbar after login dan isi logout nya sudah aman UI sama logic nya tapi masih error fungsinya

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,28 +1,7 @@
 <template>
   <div class="dashboard">
-    
-<b-navbar toggleable="lg" type="light">
-      <b-navbar-brand href="#">
-        <img src="../assets/images/logo_fatture.png" width="150px" />
-      </b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <ul class="navbar-nav ml-auto mr-3">
-        <li class="nav-item">
-          <router-link class="nav-link" to="/"> Home </router-link>
-        </li>
+    <NavbarLogin />
 
-        <b-dropdown
-          id="dropdown-right"
-          right
-          text="Username"
-          variant="outline-dark"
-          class="m-2"
-          size="sm"
-        >
-          <b-dropdown-item href="/">Logout</b-dropdown-item>
-        </b-dropdown>
-      </ul>
-    </b-navbar>
     <br />
 
     <TableVue />
@@ -32,12 +11,14 @@
 
 <script>
 import TableVue from "@/components/TableVue.vue";
+import NavbarLogin from "@/components/NavbarLogin.vue";
 
 
  export default {
     name: "dashboardPage",
     components: {
-    TableVue
+    TableVue,
+    NavbarLogin,
 },
 };
 </script>
--- src/views/UploadsView.vue
+++ src/views/UploadsView.vue
@@ -1,27 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="light">
-      <b-navbar-brand href="#">
-        <img src="../assets/images/logo_fatture.png" width="150px" />
-      </b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <ul class="navbar-nav ml-auto mr-3">
-        <li class="nav-item">
-          <router-link class="nav-link" to="/"> Home </router-link>
-        </li>
-
-        <b-dropdown
-          id="dropdown-right"
-          right
-          text="Username"
-          variant="outline-dark"
-          class="m-2"
-          size="sm"
-        >
-          <b-dropdown-item href="/">Logout</b-dropdown-item>
-        </b-dropdown>
-      </ul>
-    </b-navbar>
+    <NavbarLogin />
     <br />
 
     <div class="container">
@@ -71,10 +50,13 @@
 </template>
 
 <script>
+import NavbarLogin from '@/components/NavbarLogin.vue';
 
 export default {
     name: "UploadFiles",
-    components: {  }
+    components: { 
+      NavbarLogin, 
+      }
 };
 </script>
 
